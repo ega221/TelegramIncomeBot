7205a5063545bfc14859cfb53065376bdd0dee16
Alekseenko Egor <ega223ww@gmail.com>
2024-02-26 20:03:40 +0500

    Тест исправлен

--- tests/test_simplecalculator.py
+++ tests/test_simplecalculator.py
@@ -4,8 +4,10 @@ from utils.SimpleCalculator import add
 def test_adding_two_numbers():
     assert add(1, 2) == 3
 
+
 def test_adding_another_two_numbers():
-    assert add(2,3) == 5
+    assert add(2, 3) == 5
+
 
 def test_adding_more_numbers():
     assert add(5, 7) == 12
