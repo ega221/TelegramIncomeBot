41744a4467ed509336758121243db825d860a5d1
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-02-27 22:53:49 +0300

    добавлен файл запуска бота

---
+++ run_bot.py
@@ -0,0 +1,26 @@
+import asyncio
+import datetime
+import os
+from dotenv import load_dotenv
+from app.bot import Bot
+
+load_dotenv()
+
+
+def run():
+    """Метод для запуска бота."""
+    loop = asyncio.get_event_loop()
+    bot = Bot(os.getenv("API_TOKEN"), 2)
+
+    try:
+        print('bot has been started')
+        loop.create_task(bot.start())
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("\nstopping", datetime.datetime.now())
+        loop.run_until_complete(bot.stop())
+        print('bot has been stopped', datetime.datetime.now())
+
+
+if __name__ == '__main__':
+    asyncio.run(run())
