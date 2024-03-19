72f7ddb4ef435b51fee0d564820be43431d27eef
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-03-20 01:21:08 +0300

    добавлены задачи к asyncio

--- app/worker.py
+++ app/worker.py
@@ -4,6 +4,13 @@ from typing import List
 from client.client import TgClient
 
 
+async def delay(delay_seconds: int, message: str) -> int:
+    print(f'засыпаю на {delay_seconds} сек с сообщения {message}')
+    await asyncio.sleep(delay_seconds)
+    print(f'сон в течение {delay_seconds} сек с сообщения {message} закончился')
+    return delay_seconds
+
+
 class Worker:
     """Обработка задач из очереди"""
     def __init__(self, token: str, queue: asyncio.Queue, concurrent_workers: int):
@@ -14,8 +21,11 @@ class Worker:
 
     async def handle_update(self, upd):
         """Неприрывное считывание сообщений из бобота"""
-        if upd['message']['text'] == '/test':
-            await self.tg_client.send_message(upd['message']['chat']['id'], 'Привет!\nЯ Бобот.\nЯ научился обрабатывать одну команду!')
+        # print(upd)
+        await delay(10, upd['message']['text'])
+        # await asyncio.create_task(delay(3, upd['message']['text']))
+        # if upd['message']['text'] == '/test':
+        #     await self.tg_client.send_message(upd['message']['chat']['id'], 'Привет!\nЯ Бобот.\nЯ научился обрабатывать одну команду!')
 
     async def _worker(self):
         while True:
@@ -31,6 +41,9 @@ class Worker:
 
     async def stop(self):
         """Остановка бота"""
+        # try:
+        #     await asyncio.wait_for(self.queue.join(), timeout=60)
+        # finally:
         await self.queue.join()
         for t in self._tasks:
             t.cancel()
