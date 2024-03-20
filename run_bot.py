30a0f101298954320d2d25f45891534b0d960dfc
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-03-20 23:20:24 +0300

    добавлено nest_asyncio и удалены лишние аргументы из класса бота

--- run_bot.py
+++ run_bot.py
@@ -1,32 +1,30 @@
 """Pylint просит докстринг к импортам"""
+
 import asyncio
 import datetime
 import os
+import nest_asyncio
 from dotenv import load_dotenv
 from app.bot import Bot
-import nest_asyncio
 
 nest_asyncio.apply()
 load_dotenv()
 
 
-
 async def run():
     """Метод для запуска бота."""
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     loop = asyncio.get_event_loop()
-    bot = Bot(os.getenv("API_TOKEN"), 10)
+    bot = Bot(os.getenv("API_TOKEN"))
 
     try:
-        print('bot has been started')
+        print("bot has been started")
         loop.create_task(bot.start())
         loop.run_forever()
     except KeyboardInterrupt:
         print("\nstopping", datetime.datetime.now())
         loop.run_until_complete(bot.stop())
-        print('bot has been stopped', datetime.datetime.now())
-    
+        print("bot has been stopped", datetime.datetime.now())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(run())
