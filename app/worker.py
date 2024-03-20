960bd7b6b2f8f24a1e19afc04b988b8db8826a53
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-03-20 23:19:26 +0300

    await разнесены на задачи, исправлена асинхронность, удалены конкурентные воркеры

--- app/worker.py
+++ app/worker.py
@@ -1,49 +1,47 @@
 """Pylint просит докстринг к импортам"""
+
 import asyncio
-from typing import List
 from client.client import TgClient
 
-
 async def delay(delay_seconds: int, message: str) -> int:
-    print(f'засыпаю на {delay_seconds} сек с сообщения {message}')
+    print(f"засыпаю на {delay_seconds} сек с сообщения {message}")
     await asyncio.sleep(delay_seconds)
-    print(f'сон в течение {delay_seconds} сек с сообщения {message} закончился')
+    print(f"сон в течение {delay_seconds} сек с сообщения {message} закончился")
     return delay_seconds
 
 
 class Worker:
     """Обработка задач из очереди"""
-    def __init__(self, token: str, queue: asyncio.Queue, concurrent_workers: int):
-        self.tg_client = TgClient(token)
-        self.queue = queue
-        self.concurrent_workers = concurrent_workers
-        self._tasks: List[asyncio.Task] = []
 
-    async def handle_update(self, upd):
-        """Неприрывное считывание сообщений из бобота"""
-        # print(upd)
-        await delay(10, upd['message']['text'])
-        # await asyncio.create_task(delay(3, upd['message']['text']))
-        # if upd['message']['text'] == '/test':
-        #     await self.tg_client.send_message(upd['message']['chat']['id'], 'Привет!\nЯ Бобот.\nЯ научился обрабатывать одну команду!')
+    def __init__(self, queue: asyncio.Queue, tg_client: TgClient):
+        self.tg_client = tg_client
+        self.queue = queue
+        self._task: asyncio.Task = None
+
+    async def _do_task(self, upd):
+        """Метод, который передает информацию апдейта в другие серсивы"""
+        try:
+            task = asyncio.create_task(delay(3, upd["message"]["text"]))
+            await task
+            # if upd['message']['text'] == '/test':
+            # await self.tg_client.send_message(upd['message']['chat']['id'], 'Привет!\nЯ Бобот.\nЯ научился обрабатывать одну команду!')
+        finally:
+            self.queue.task_done()
 
     async def _worker(self):
+        """Работяга, который достает апдейты из очереди и запускает асинхронную задачу"""
         while True:
-            upd = await self.queue.get()
-            try:
-                await self.handle_update(upd)
-            finally:
-                self.queue.task_done()
+            task_get_upd = self.queue.get()
+            upd = await task_get_upd
+            asyncio.create_task(self._do_task(upd))
 
     async def start(self):
-        """Запуск добавления задач в очереди"""
-        self._tasks = [asyncio.create_task(self._worker()) for _ in range(self.concurrent_workers)]
+        """Запуск воркера"""
+        self._task = asyncio.create_task(self._worker())
 
     async def stop(self):
-        """Остановка бота"""
-        # try:
-        #     await asyncio.wait_for(self.queue.join(), timeout=60)
-        # finally:
-        await self.queue.join()
-        for t in self._tasks:
-            t.cancel()
+        """Метод, который останавливает воркер и прекращает задачу"""
+        try:
+            await asyncio.wait_for(self.queue.join(), timeout=60)
+        finally:
+            self._task.cancel()
