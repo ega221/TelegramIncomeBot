77868eef3de0d40b738ae0f34643be4703e7944c
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-03-27 11:10:51 +0300

    Добавлены нужные аргументы в класс и очередь исправлена на ограниченную

--- app/poller.py
+++ app/poller.py
@@ -3,20 +3,23 @@
 import asyncio
 from client.client import TgClient
 
+
 class Poller:
     """Получение апдейтов из телеграма и добавление их в очередь"""
 
-    def __init__(self, queue: asyncio.Queue, tg_client: TgClient):
+    def __init__(self, queue: asyncio.Queue, tg_client: TgClient, update_timeout: int):
         self.tg_client = tg_client
         self.queue = queue
+        self.update_timeout = update_timeout
         self._task: asyncio.Task = None
+        
 
     async def _worker(self):
         """Работяга, который выполняет получение апдейтов из телеграма и добавлением их в очередь"""
         offset = 0
         while True:
             task = asyncio.create_task(
-                self.tg_client.get_updates_in_objects(offset=offset, timeout=60)
+                self.tg_client.get_updates_in_objects(offset=offset, timeout=self.update_timeout)
             )
             res = await task
             for upd in res["result"]:
