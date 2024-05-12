1a02185fe2f125b9aae73da55a563fd01d636be2
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2024-05-12 22:43:53 +0300

    Инициализация репозиториев

--- run_bot.py
+++ run_bot.py
@@ -1,19 +1,46 @@
 """Pylint просит докстринг к импортам"""
 
 import asyncio
+import asyncpg
 import datetime
 import os
 import nest_asyncio
 from dotenv import load_dotenv
 from app.bot import Bot
+from repository.expense_category_repository.expense_category_repository_impl import (
+    ExpenseCategoryRepositoryImpl,
+)
+from repository.expense_repository.expense_repository_impl import ExpenseRepositoryImpl
+from repository.income_category_repository.income_category_repository_impl import (
+    IncomeCategoryRepositoryImpl,
+)
+from repository.income_repository.income_repository_impl import IncomeRepositoryImpl
+from repository.user_repository.user_repository_impl import UserRepositoryImpl
 
 nest_asyncio.apply()
 load_dotenv()
 
 
+async def create_pool():
+    return await asyncpg.create_pool(
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
+        port=os.getenv("PG_PORT"),
+        host=os.getenv("PG_HOST"),
+    )
+
+
 async def run():
     """Метод для запуска бота."""
     loop = asyncio.get_event_loop()
+    pool = await create_pool()
+
+    user_repository = UserRepositoryImpl(pool)
+    expense_category_repository = ExpenseCategoryRepositoryImpl(pool)
+    expense_repository = ExpenseRepositoryImpl(pool)
+    income_category_repository = IncomeCategoryRepositoryImpl(pool)
+    income_repository = IncomeRepositoryImpl(pool)
 
     bot = Bot(
         token=os.getenv("API_TOKEN"),
