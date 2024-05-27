0e1dbb1bb48bc53512ecb635f6c2f48ac490177a
Timur <timurkashapov15@gmail.com>
2024-05-28 00:06:20 +0300

    добавлен интерфейс для сервисов

--- services/interface.py
+++ services/interface.py
@@ -1,20 +1,23 @@
 from datetime import datetime
+from model.income import Income
+from model.expense import Expense
+from typing import Union
 
 
 class IncomeService:
     async def initiate(self, user_id: int):
         pass
 
-    async def set_category(self, user_id: int, category_id: int):
+    async def set_category(self, user_id: int, category_id: int) -> Union[Income, Expense]:
         pass
 
-    async def set_date(self, user_id: int, date: datetime):
+    async def set_date(self, user_id: int, date: datetime) -> Union[Income, Expense]:
         pass
 
-    async def set_value(self, user_id: int, value: int):
+    async def set_value(self, user_id: int, value: int) -> Union[Income, Expense]:
         pass
 
-    async def save(self, user_id: int):
+    async def save(self, user_id: int) -> Union[Income, Expense]:
         pass
 
     async def drop(self, user_id: int) -> None:
