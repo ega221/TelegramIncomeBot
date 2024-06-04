c23f9c1fe8a11c14cfdb6ce0324c8bccd1e7b6cf
Боринских Семен Денисович <borinskihsd@sovcombank.ru>
2024-06-04 13:59:43 +0300

    Инициализированы валидатор для даты, чисел и категорий

---
+++ validators/category_validator.py
@@ -0,0 +1,29 @@
+"""Декоратор для валидации входящей категории"""
+
+import asyncio
+from typing import Awaitable
+
+
+def is_string_with_letters(argument: str):
+    """Метод проверяет, что входящий аргумент является строкой"""
+    return isinstance(argument, str) and argument.isalpha()
+
+
+def category_validator(coroutine_func: Awaitable):
+    """Декоратор для валидации строки с категорией.
+    Он проверяет, что в строке содержатся только буквы"""
+
+    async def wrapper(*args, **kwargs):
+        """Асинхронная свертка для корутины"""
+        if (
+            not kwargs
+            or not isinstance(kwargs["category"], str)
+            or not is_string_with_letters(kwargs["category"])
+        ):
+            raise ValueError("Категория должна быть строкой, содержащей только буквы")
+        task = asyncio.create_task(coroutine_func(*args, **kwargs))
+        result = await task
+
+        return result
+
+    return wrapper
---
+++ validators/number_validator.py
@@ -0,0 +1,33 @@
+"""Декоратор для валидации входящего значения Income"""
+
+import asyncio
+from typing import Awaitable
+
+
+def is_number(argument: str):
+    """Метод проверяет, что входное значение является int или float числом"""
+    try:
+        _ = float(argument)
+        return True
+    except ValueError:
+        return False
+
+
+def validate_number(coroutine_func: Awaitable):
+    """Декоратор для асинхронной функции.
+    Используется для валидации того, что входной аргумент = число"""
+
+    async def wrapper(*args, **kwargs):
+        """Асинхронная свертка для корутины"""
+        if (
+            not kwargs
+            or not isinstance(kwargs["date"], str)
+            or not is_number(kwargs["date"])
+        ):
+            raise ValueError("Значение должно быть формата 12345.67 или 12345")
+        task = asyncio.create_task(coroutine_func(*args, **kwargs))
+        result = await task
+
+        return result
+
+    return wrapper
