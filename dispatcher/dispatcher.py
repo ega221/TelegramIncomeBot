57af32643edb78b73563c84b892d023969b13e2b
Semen Borinskikh <ph.borinskikh@mail.ru>
2024-06-07 20:28:41 +0300

    Исправлен диспетчер

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -4,11 +4,13 @@ import asyncio
 
 from config.command_list import CommandsEnum
 from config.status_list import StatusEnum
+from model.messages import Message
 from model.tg_update import Update
 from services.expense_service.expense_service_impl import ExpenseServiceImpl
 from services.income_service.income_service_impl import IncomeServiceImpl
 from services.interface import Service
-from user_cache.interface import UserCache
+from services.user_service.user_service_impl import UserServiceImpl
+from user_cache.cache.user_cache_impl import UserCacheImpl
 
 
 class Dispatcher:
@@ -21,11 +23,13 @@ class Dispatcher:
         self,
         income_service: IncomeServiceImpl,
         expense_service: ExpenseServiceImpl,
-        user_cache: UserCache,
+        user_cache: UserCacheImpl,
+        user_service: UserServiceImpl,
     ) -> None:
         self.state_machine = None
         self.income_service = income_service
         self.expense_service = expense_service
+        self.user_service = user_service
         self.user_cache = user_cache
 
     async def update(self, upd: Update) -> Update:
@@ -34,46 +38,31 @@ class Dispatcher:
         """
         state = self.state_machine.get_status(upd.telegram_id)
         result = Update(telegram_id=upd.telegram_id, text="", update_id=upd.update_id)
-
+        message = Message.UNKNOWN_COMMAND
         try:
-            # Если статус = начальный и сообщение является командой
+            # Если статус = начальный и сообщение является командой /start
             if (state.status == StatusEnum.idle) and (
-                upd.text in CommandsEnum.get_commands()
+                upd.message == CommandsEnum.start
             ):
-                # Вызывается метод, инициализирующий нужный по команде сервис
-                if upd.text == CommandsEnum.cancel:
-                    # TODO: Добавить ссылку на начальное сообщение
-                    self.user_cache.drop(upd.telegram_id)
-                    message = "СООБЩЕНИЕ НАЧАЛЬНОЕ"
-                elif upd.message == CommandsEnum.start:
-                    # TODO: Вызвать метод "save" из UserService
-                    message = "СООБЩЕНИЕ НАЧАЛЬНОЕ"
-                    pass
-                else:
-                    # Иначе инициализируется работа сервиса
-                    service = self.get_service_by_command(upd.text)
-                    task = asyncio.create_task(service.initiate(upd.telegram_id))
-                    message = await task
+                self.user_service.save(upd)
+                message = Message.GREETING
+            # Если статус = начальный и сообщение является командой /start
             else:
-                # Иначе вызывается просто продолжения методов из сервисов
-                task = asyncio.create_task(state.func(upd))
-                message = await task
+                if upd.text in [CommandsEnum.make_income]:
+                    task = asyncio.create_task(self.income_service.initiate(upd))
+                    message = await task
+                elif upd.text in [CommandsEnum.make_expense]:
+                    task = asyncio.create_task(self.expense_service.initiate(upd))
+                    message = await task
+                elif upd.text == CommandsEnum.cancel:
+                    self.user_cache.drop(upd.telegram_id)
+                    message = Message.CANCEL
+                    # TODO: Сказать стейт машине что этот пользователь = начальное состояние
+                else:
+                    task = asyncio.create_task(state.func(upd))
+                    message = await task
         except Exception:
             # TODO: Отлавливать какие-то конкретные исключения
             pass
         result.text = message
         return result
-
-    async def get_service_by_command(self, command: str) -> Service:
-        """Возвращает сервис, который необходимо инициализировать"""
-        income_list = [CommandsEnum.make_income]
-        expense_list = [CommandsEnum.make_expense]
-
-        service = None
-        if command in income_list:
-            service = self.income_service
-        elif command in expense_list:
-            service = self.expense_service
-        else:
-            raise KeyError("Команда неизвестная")
-        return service
