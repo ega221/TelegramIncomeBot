418d459e0f49661d0f133adb42ab6bddbf695525
Боринских Семен Денисович <borinskihsd@sovcombank.ru>
2024-06-07 16:46:19 +0300

    Подкорректирован шаблон диспетчера

--- dispatcher/dispatcher.py
+++ dispatcher/dispatcher.py
@@ -4,7 +4,11 @@ import asyncio
 
 from config.command_list import CommandsEnum
 from config.status_list import StatusEnum
-from model.response_templates import Update
+from model.tg_update import Update
+from services.expense_service.expense_service_impl import ExpenseServiceImpl
+from services.income_service.income_service_impl import IncomeServiceImpl
+from services.interface import Service
+from user_cache.interface import UserCache
 
 
 class Dispatcher:
@@ -13,61 +17,63 @@ class Dispatcher:
     и вызывает нужный серсив
     """
 
-    def __init__(self) -> None:
-        # TODO: Добавить машину
+    def __init__(
+        self,
+        income_service: IncomeServiceImpl,
+        expense_service: ExpenseServiceImpl,
+        user_cache: UserCache,
+    ) -> None:
         self.state_machine = None
+        self.income_service = income_service
+        self.expense_service = expense_service
+        self.user_cache = user_cache
 
-    async def update(self, upd):
+    async def update(self, upd: Update) -> Update:
         """Метод, который обращается к state machine
         и перенаправляет update в соответствующий сервис
         """
-        # state = self.state_machine.get_status(upd["chat_id"])
-        result = upd
+        state = self.state_machine.get_status(upd.chat_id)
+        result = Update(chat_id=upd.chat_id, text="", update_id=upd.update_id)
 
-        # try:
-        #     # Если статус = начальный и сообщение является командой
-        #     if (state.status == StatusEnum.idle) and (
-        #         upd.message in Commands.get_commands()
-        #     ):
-        #         # Вызывается метод, инициализирующий нужный по команде сервис
-        #         if upd.message == Commands.cansel:
-        #             # TODO: Возвращаем все состояние
-        #             pass
-        #         elif upd.message == Commands.start:
-        #             # TODO: Вызвать метод "start"
-        #             pass
-        #         else:
-        #             # Иначе инициализируется работа сервиса
-        #             service = self.get_service_by_command(upd.message)
-        #             task = asyncio.create_task(service.initiate(upd["chat_id"]))
-        #             result = await task
-        #     else:
-        #         # Иначе вызывается просто продолжения методов из сервисов
-        #         task = asyncio.create_task(self.to_service(state, upd))
-        #         result = await task
-        # except Exception as e:
-        #     result = e
-
-        return Update(chat_id=upd.chat_id, text=result.text, update_id=upd.update_id)
-
-    @staticmethod
-    async def to_service(state, upd):
-        """Отправка update в нужный сервис"""
-        task = asyncio.create_task(state.func(upd))
-        result = await task
-
+        try:
+            # Если статус = начальный и сообщение является командой
+            if (state.status == StatusEnum.idle) and (
+                upd.text in CommandsEnum.get_commands()
+            ):
+                # Вызывается метод, инициализирующий нужный по команде сервис
+                if upd.text == CommandsEnum.cansel:
+                    # TODO: Добавить ссылку на начальное сообщение
+                    self.user_cache.drop(upd.chat_id)
+                    message = "СООБЩЕНИЕ НАЧАЛЬНОЕ"
+                elif upd.message == CommandsEnum.start:
+                    # TODO: Вызвать метод "save" из UserService
+                    message = "СООБЩЕНИЕ НАЧАЛЬНОЕ"
+                    pass
+                else:
+                    # Иначе инициализируется работа сервиса
+                    service = self.get_service_by_command(upd.text)
+                    task = asyncio.create_task(service.initiate(upd.chat_id))
+                    message = await task
+            else:
+                # Иначе вызывается просто продолжения методов из сервисов
+                task = asyncio.create_task(state.func(upd))
+                message = await task
+        except Exception:
+            # TODO: Отлавливать какие-то конкретные исключения
+            pass
+        result.text = message
         return result
 
-    @staticmethod
-    async def get_service_by_command(command: str):
+    async def get_service_by_command(self, command: str) -> Service:
         """Возвращает сервис, который необходимо инициализировать"""
-        income_list = [CommandsEnum.make_income, CommandsEnum.get_income_categories]
-        expense_list = [CommandsEnum.make_expense, CommandsEnum.get_expense_categories]
+        income_list = [CommandsEnum.make_income]
+        expense_list = [CommandsEnum.make_expense]
 
         service = None
         if command in income_list:
-            service = IncomeService
+            service = self.income_service
         elif command in expense_list:
-            service = ExpenseService
-
+            service = self.expense_service
+        else:
+            raise KeyError("Команда неизвестная")
         return service
